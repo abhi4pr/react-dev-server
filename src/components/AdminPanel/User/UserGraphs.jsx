70037e6f06fe58e87e0ad559a6216765ea42471b
abhi4pr <ascs739@gmail.com>
2023-12-20 18:50:58 +0530

    changed api url

--- src/components/AdminPanel/User/UserGraphs.jsx
+++ src/components/AdminPanel/User/UserGraphs.jsx
@@ -12,7 +12,7 @@ const UserGraphs = () => {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3000/api/get_user_graph_data",{
+      .post("http://34.93.221.166:3000/api/get_user_graph_data",{
         caseType:"gender"
       })
       .then((res) => {
@@ -24,7 +24,7 @@ const UserGraphs = () => {
     const newFilter = e.target ? e.target.value : e;
     setSelectedFilter(newFilter);
     await axios
-      .post("http://localhost:3000/api/get_user_graph_data",{
+      .post("http://34.93.221.166:3000/api/get_user_graph_data",{
         caseType:e.target.value
       })
       .then((res) => {
