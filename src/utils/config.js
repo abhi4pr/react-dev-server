99ce2f79fb84f74fbcc595b2da699004de172a14
Nagesh <nageshslnk94@gmail.com>
2024-02-09 19:07:10 +0530

    bug fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export const baseUrl = "http://34.93.221.166:3000/api/"; // for development
+// export const baseUrl = "http://192.168.1.29:3000/api/"; // for development
 // export const baseUrl = "https://jarvis.work/api/"; // for production
 
 // export const baseUrl = "http://192.168.1.5:3000/api/"; //Bhaiyu Dada
