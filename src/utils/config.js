ca18e26134a609710f457aafebe77d24521c4f95
Lalit <goursumit52@gmail.com>
2024-03-06 12:43:26 +0530

    canges assst

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://34.173.148.74:8080/api/"; // for development
+// export const baseUrl = "http://34.173.148.74:8080/api/"; // for development
 // export const baseUrl = "http://192.168.1.17:3000/api/"; // for development
 // export const baseUrl = "https://jarvis.work/api/"; // for production
 
-// export const baseUrl = "http://192.168.1.27:8080/api/"; //Bhaiyu Dada
+export const baseUrl = "http://192.168.1.27:8080/api/"; //Bhaiyu Dada
