c18cd4c7c7c644a636b6a5de5c6bd50d47a21b3f
palak chouhan <palakchouhan2000@gmail.com>
2024-04-04 06:41:57 -0700

    changes in hide unhide row logic

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export const baseUrl = "http://34.173.148.74:8080/api/"; // for development
+
 // export const baseUrl = "http://192.168.1.17:3000/api/"; // for development
 // export const baseUrl = "http://34.69.14.1:8080/api/"; // for production
 
@@ -6,4 +7,4 @@ export const baseUrl = "http://34.173.148.74:8080/api/"; // for development
 // export const baseUrl = "http://192.168.1.45:8080/api/";
 //export const baseUrl = "http://192.168.1.7:8080/api/"; //Bhaiyu Dada
 //   export const baseUrl = "http://192.168.1.58:8080/api/"; //Ankita new
-// export const baseUrl = "http://192.168.1.8:8080/api/"; //Aman new
+// export const baseUrl = "http://192.168.1.31:8080/api/"; // for development
