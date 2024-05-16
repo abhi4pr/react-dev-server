98022b9de4fe0622025e2923ac7dee635edbf3e0
abhi4pr <ascs739@gmail.com>
2024-05-16 13:23:41 +0530

    admin, super admin hide in role dropdown, further date disable

--- src/components/AdminPanel/User/UserMaster.jsx
+++ src/components/AdminPanel/User/UserMaster.jsx
@@ -48,6 +48,7 @@ import { User } from "@phosphor-icons/react";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { subYears } from "date-fns";
+import dayjs from 'dayjs';
 
 const colourOptions = [
   { value: "English", label: "English" },
@@ -234,6 +235,7 @@ const UserMaster = () => {
   const token = sessionStorage.getItem("token");
   const decodedToken = jwtDecode(token);
   const loginUserId = decodedToken.id;
+  const ROLEID = decodedToken.role_id;
 
   const [familyValidationErrors, setFamilyValidationErrors] = useState({});
 
@@ -346,6 +348,15 @@ const UserMaster = () => {
       setCityData(res.data.data);
     });
   }, []);
+
+  // to disable admin and super admin in role dropdown start
+  const modifiedRoleData = roledata.map((option) => ({
+    value: option.role_id,
+    label: option.Role_name,
+    isDisabled: ROLEID === 1 && (option.role_id === 1 || option.role_id === 6),
+  }));
+  const selectedRole = roledata.find((role) => role.role_id === roles);
+  // to disable admin and super admin in role dropdown end
 
   useEffect(() => {
     axios.get(baseUrl + "get_all_hobbies").then((res) => {
@@ -1315,7 +1326,19 @@ const UserMaster = () => {
     return days;
   }
 
-  const handleDateChange = (e) => {
+  const handleDateChange = (e, newDate) => {
+    // const today = dayjs();
+    // const minDate = today.subtract(15, 'year');
+    // const selectedDateN = dayjs(newDate);
+
+    // if (selectedDateN.isValid() && selectedDateN.isBefore(minDate)) {
+    //   console.log('Date Set');
+    // } else {
+    //   alert('Age must be at least 15 years');
+    //   setDateOfBirth('')
+    //   return
+    // }
+
     const selectedDate = e;
     const age = calculateAge(selectedDate);
     const ageDays = calculateAgeInDays(selectedDate);
@@ -1657,6 +1680,7 @@ const UserMaster = () => {
             value={dateOfBirth}
             onChange={handleDateChange}
             renderInput={(params) => <TextField {...params} />}
+            maxDate={dayjs()}
           />
         </LocalizationProvider>
       </div>
@@ -2049,19 +2073,12 @@ const UserMaster = () => {
           Role <sup className="form-error">*</sup>
         </label>
         <Select
-          options={roledata.map((option) => ({
-            value: option.role_id,
-            label: option.Role_name,
-          }))}
-          value={{
-            value: roles,
-            label:
-              roledata.find((role) => role.role_id === roles)?.Role_name || "",
-          }}
+          options={modifiedRoleData}
+          value={selectedRole ? { value: selectedRole.role_id, label: selectedRole.Role_name } : null}
           onChange={(e) => {
             setRoles(e.value);
           }}
-        ></Select>
+        />
       </div>
       <div className="col-md-3">
         <FieldContainer
