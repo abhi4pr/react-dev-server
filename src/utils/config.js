491e5b4579fa282d090c8055d179f19f0381ab66
ManojGavel <manojgavel1@gmail.com>
2024-06-12 16:15:56 +0530

    config dev uncommit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-// export const baseUrl = "http://34.173.148.74:8080/api/"; // for development
+export const baseUrl = "http://34.173.148.74:8080/api/"; // for development
 // export const baseUrl = "http://34.69.14.1:8080/api/"; // for production
 // export const baseUrl = "https://jarvis.work:8080/api/"; // for production
 // export const baseUrl = "http://jarvis.work:8080/api/"; // for production
@@ -6,6 +6,6 @@
 // export const baseUrl = "http://192.168.29.94:8080/api/"; //Bhaiyu Dada
 // export const baseUrl = "http://192.168.29.143:8080/api/"; //Ankita new
 // export const baseUrl = "http://192.168.29.58:8080/api/"; //Aman new
-export const baseUrl = "http://127.0.0.1:8080/api/"; //Localhost
+// export const baseUrl = "http://127.0.0.1:8080/api/"; //Localhost
 // export const baseUrl = "http://192.168.29.63:8080/api/"; //Sumit
 // export const baseUrl = "http://192.168.29.46:8080/api/"; //Sumit
