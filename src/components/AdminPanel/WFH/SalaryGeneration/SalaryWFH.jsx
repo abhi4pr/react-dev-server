e0c4b778885c01247aadb83301955944938cc550
lalit gour <goursumit52@gmail.com>
2024-07-12 11:31:19 +0530

    changes on user and wfhd

--- src/components/AdminPanel/WFH/SalaryGeneration/SalaryWFH.jsx
+++ src/components/AdminPanel/WFH/SalaryGeneration/SalaryWFH.jsx
@@ -1307,7 +1307,8 @@ const SalaryWFH = () => {
           </div>
 
           <h6 style={{ color: "green", paddingTop: "10px" }}>
-            <span>Active : {activeusers}</span>
+            {/* <span>Active : {activeusers}</span> */}
+            <span>Active : {filterData?.length}</span>
           </h6>
         </div>
       </div>
